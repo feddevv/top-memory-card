717d111ddc58086b70faf73ab82881792983038f
Fedya Styopochkin <fst20071221@gmail.com>
2026-05-26 14:38:53 +0000

    feat: add fonts and main background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import { useFetch } from './hooks/useFetch'
+import './styles/index.css'
 
 function App() {
   const {isLoad, error, data} = useFetch("https://pokeapi.co/api/v2/pokemon/ditto")
 
-  console.log(isLoad)
-  
   return (
     <div>
       {isLoad ? <p>Loading...</p> : <h1>Hello World123!</h1>}
